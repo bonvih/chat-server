499bdaf719f4f9979ec5b300b141b2f0780c5bf7
toundaherve <toundaherve@gmail.com>
2022-01-08 14:26:06 +0000

    Refactorings

--- serverWebSocket.js
+++ serverWebSocket.js
@@ -8,38 +8,20 @@ function initServerWebSocket(serverHTTP) {
         }
     })
     
-    io.on("connection", (socket) => {            
-        socket.on("set userID", (userID) => {
-            updatePresence(userID, "online")
-
-            if(!userID) {
-                console.log("WebSocketServer")
-                console.log("Event: set userID")
-                console.log("Missing userID")
-                console.log("\n")
-                return
-            }
-
-            socket.userID = userID
+    io.on("connection", (socket) => {
+        socket.on("set_profile", profile => {
+            socket.profile = profile
         })
 
-        socket.on("private message", async (message) => {
+        socket.on("join_room", room => {
+            socket.join(room)
+        })
+
+        socket.on("private_message", async (message, room) => {
             try {
                await storeMessage(message)
-               const sockets = await io.fetchSockets()
 
-               const recipientID = message.recipientID+message.propositionID
-
-               const recipientSocket = sockets.find(s => s.userID === recipientID)
-                if (recipientSocket) {
-                    socket.to(recipientSocket.id).emit("private message", message)
-                } else {
-                    console.log("WebSocketServer")
-                    console.log("Event: private message")
-                    console.log("Couldn't find " + recipientID + " socket")
-                    console.log("\n")
-                }
-
+               socket.to(room).emit("private_message", message)
             } catch (error) {
                 console.log("WebSocketServer")
                 console.log("Event: private message")
@@ -48,54 +30,41 @@ function initServerWebSocket(serverHTTP) {
             }
         })
 
-        socket.on("typing", async (typing, toUserID) => {
-            try {
-                const sockets = await io.fetchSockets()
-                const recipientSocket = sockets.find(s => s.userID === toUserID)
-                if (recipientSocket) {
-                    socket.to(recipientSocket.id).emit("typing", typing)
-                } else {
-                    console.log("WebSocketServer")
-                    console.log("Event: typing")
-                    console.log("Couldn't find " + toUserID + " socket")
-                    console.log("\n")
+        socket.on("typing", async (typing, room) => {
+            socket.to(room).emit("typing", typing)
+        })
+
+        socket.on("disconnecting", () => {
+            if (socket.profile) {
+                if(socket.rooms.has(socket.profile.ID.toString())) {
+                    socket.rooms.forEach(room => {
+                        socket.to(room).emit("online_status", false)
+                    })
                 }
-            } catch (error) {
-                console.log("WebSocketServer")
-                console.log("Event: typing")
-                console.log("Error: ", error)
-                console.log("\n")
             }
         })
 
-        socket.on("disconnect", reason => {
-            socket.to(socket.userID).emit("online status update", false)
-
-            console.log("WebSocketServer")
-            console.log("Event: Disconnect")
-            console.log("UserID: ", socket.userID)
-            console.log("Reason: ", reason)
-            console.log("\n")
-
-            updatePresence(socket.userID, "offline")
+        socket.on("online_status", (status) => {
+            socket.rooms.forEach(room => {
+                socket.to(room).emit("online_status", status)
+            })
         })
 
-        socket.on("join room", room => {
-            socket.join(room)
-        })
-
-        socket.on("online status update", status => {
-            socket.to(socket.userID).emit("online status update", status)
-        })
-
-        socket.on("online status enquiry", async (userID) => {
+        socket.on("get_online_status", async (profileID) => {
             try {
                 const sockets = await io.fetchSockets()
-                const recipientSocket = sockets.find(s => s.userID === userID)
-                if (recipientSocket) {
-                    io.to(socket.id).emit("online status update", true)
+
+                const foundSocket = sockets.find(s => {
+                    if (s.profile) {
+                       return s.profile.ID.toString() === profileID
+                    }
+                    return false;
+                })
+                
+                if (foundSocket) {
+                    io.to(socket.id).emit("online_status", true)
                 } else {
-                    io.to(socket.id).emit("online status update", false)
+                    io.to(socket.id).emit("online_status", false)
                 }
             } catch (error) {
                 console.log("WebSocketServer")
@@ -108,13 +77,7 @@ function initServerWebSocket(serverHTTP) {
 }
 
 async function storeMessage(message) {
-    console.log("Storing message\n")
     return Promise.resolve("Message stored")
-}
-
-async function updatePresence(userID, status) {
-    console.log("Updating user" + userID + " presence to " + status + "\n")
-    return Promise.resolve("Presence updated")
 }
 
 module.exports = initServerWebSocket
