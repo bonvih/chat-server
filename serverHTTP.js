19bbe2f75f820b40626ab8c131e9c6129d98bcb1
toundaherve <toundaherve@gmail.com>
2022-02-09 09:35:40 +0000

    More fixes

--- serverHTTP.js
+++ serverHTTP.js
@@ -1,7 +1,32 @@
 const express = require("express")
 const {createServer} = require("http")
+const cors = require("cors")
+const WebServiceClient = require("@maxmind/geoip2-node").WebServiceClient
+
+const geoipAccountID = ''
+const geoipLicenseKey = ''
+
+const geoipClient = new WebServiceClient(geoipAccountID, geoipLicenseKey)
 
 const app = express()
+app.use(cors())
+
+app.get("/geoip", (req, res) => {
+    const ip = req.query.ip
+
+    if (!ip) {
+        res.status(400).send("Missing ip")
+        return
+    }
+
+    geoipClient.city(ip).then(response => {
+        console.log(response)
+        res.status(200).json(response)
+    }).catch(error => {
+        console.log(error)
+        res.status(500).send("Internal server error")
+    })
+})
 
 function initServerHTTP() {
     return createServer(app)
--- serverWebSocket.js
+++ serverWebSocket.js
@@ -1,84 +1,84 @@
-const { Server } = require("socket.io")
-const axios = require("axios")
+const { Server } = require("socket.io");
+const axios = require("axios");
 
 function initServerWebSocket(serverHTTP) {
-    const io = new Server(serverHTTP, {
-        cors: {
-            origin: "*",
-            methods: ["GET", "POST"]
+  const io = new Server(serverHTTP, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("set_profile", (profile) => {
+      socket.profile = profile;
+    });
+
+    socket.on("join_room", (room) => {
+      socket.join(room);
+    });
+
+    socket.on("private_message", async (message, room) => {
+      try {
+        const response = await storeMessage(message);
+
+        socket.to(room).emit("private_message", message);
+      } catch (error) {
+        console.log("WebSocketServer");
+        console.log("Event: private message");
+        console.log("Error: ", error);
+        console.log("\n");
+      }
+    });
+
+    socket.on("typing", async (typing, room) => {
+      socket.to(room).emit("typing", typing);
+    });
+
+    socket.on("disconnecting", () => {
+      if (socket.profile) {
+        if (socket.rooms.has(socket.profile.ID.toString())) {
+          socket.rooms.forEach((room) => {
+            socket.to(room).emit("online_status", false);
+          });
         }
-    })
-    
-    io.on("connection", (socket) => {
-        socket.on("set_profile", profile => {
-            socket.profile = profile
-        })
+      }
+    });
 
-        socket.on("join_room", room => {
-            socket.join(room)
-        })
+    socket.on("online_status", (status) => {
+      socket.rooms.forEach((room) => {
+        socket.to(room).emit("online_status", status);
+      });
+    });
 
-        socket.on("private_message", async (message, room) => {
-            try {
-               const response = await storeMessage(message)
+    socket.on("get_online_status", async (profileID) => {
+      try {
+        const sockets = await io.fetchSockets();
 
-               socket.to(room).emit("private_message", message)
-            } catch (error) {
-                console.log("WebSocketServer")
-                console.log("Event: private message")
-                console.log("Error: ", error)
-                console.log("\n")
-            }
-        })
+        const foundSocket = sockets.find((s) => {
+          if (s.profile) {
+            return s.profile.ID.toString() === profileID;
+          }
+          return false;
+        });
 
-        socket.on("typing", async (typing, room) => {
-            socket.to(room).emit("typing", typing)
-        })
-
-        socket.on("disconnecting", () => {
-            if (socket.profile) {
-                if(socket.rooms.has(socket.profile.ID.toString())) {
-                    socket.rooms.forEach(room => {
-                        socket.to(room).emit("online_status", false)
-                    })
-                }
-            }
-        })
-
-        socket.on("online_status", (status) => {
-            socket.rooms.forEach(room => {
-                socket.to(room).emit("online_status", status)
-            })
-        })
-
-        socket.on("get_online_status", async (profileID) => {
-            try {
-                const sockets = await io.fetchSockets()
-
-                const foundSocket = sockets.find(s => {
-                    if (s.profile) {
-                       return s.profile.ID.toString() === profileID
-                    }
-                    return false;
-                })
-                
-                if (foundSocket) {
-                    io.to(socket.id).emit("online_status", true)
-                } else {
-                    io.to(socket.id).emit("online_status", false)
-                }
-            } catch (error) {
-                console.log("WebSocketServer")
-                console.log("Event: typing")
-                console.log("Error: ", error)
-                console.log("\n")
-            }
-        })
-    })
+        if (foundSocket) {
+          io.to(socket.id).emit("online_status", true);
+        } else {
+          io.to(socket.id).emit("online_status", false);
+        }
+      } catch (error) {
+        console.log("WebSocketServer");
+        console.log("Event: typing");
+        console.log("Error: ", error);
+        console.log("\n");
+      }
+    });
+  });
 }
 
 async function storeMessage(message) {
-    return axios.post("http://192.168.1.69:4000/api/messages", message)
+  return axios.post("http://192.168.1.70:4000/api/messages", message);
 }
 
-module.exports = initServerWebSocket
+module.exports = initServerWebSocket;
