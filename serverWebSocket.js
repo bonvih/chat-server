b16c22b06933d0681c579842739095d468be03a6
herves <toundaherve@gmail.com>
2022-05-16 12:54:45 +0100

    Fix receiving message

--- serverWebSocket.js
+++ serverWebSocket.js
@@ -29,7 +29,8 @@ function initServerWebSocket(serverHTTP) {
         const response = await storeMessage(message);
 
         // Other user is chatting with me ?
-        if (io.sockets.adapter.rooms.get(room).size > 3) {
+        console.log("Room size: ", io.sockets.adapter.rooms.get(room).size)
+        if (io.sockets.adapter.rooms.get(room).size > 1) {
           socket.to(room).emit("private_message", message);
         } else {
           await notifyUser(otherUserID, message)
