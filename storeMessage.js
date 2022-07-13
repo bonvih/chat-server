98712ea5a83578ccd2511969065e792285ca0920
herves <toundaherve@gmail.com>
2022-07-13 15:39:32 +0100

    Fix SCHEME

--- storeMessage.js
+++ storeMessage.js
@@ -2,8 +2,14 @@ const axios = require("axios");
 
 const API_SERVER_HOST = process.env.API_SERVER_HOST;
 
+let SCHEME = "http";
+
+if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
+  SCHEME = "https";
+}
+
 async function storeMessage(message) {
-  return axios.post("http://" + API_SERVER_HOST + "/api/messages", message);
+  return axios.post(`${SCHEME}://${API_SERVER_HOST}/messages`, message);
 }
 
 module.exports = storeMessage;
